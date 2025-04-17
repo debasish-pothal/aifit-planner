0a3351b22776efcb34fcdf0f561d69c3d4bafede
Debasish Pothal <debasish_pothal@hotmail.com>
2025-04-17 10:16:51 +0530

    added new pages

---
+++ src/app/create-plan/page.tsx
@@ -0,0 +1,7 @@
+export default function CreatePlan() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">Create Plan</h1>
+    </div>
+  );
+}
---
+++ src/app/my-profile/page.tsx
@@ -0,0 +1,7 @@
+export default function MyProfile() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">My Profile</h1>
+    </div>
+  );
+}
